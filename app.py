b984c25cf9e2bc7ce34018271fc9be7c2b012243
1m4nim <fancustard9@gmail.com>
2025-06-11 09:56:52 +0900

    画像の’プレビューが出るようにした

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask, request, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 from datetime import datetime
 import pytz
 
 app = Flask(__name__)
+
+# アップロード先のディレクトリ
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # SQLite設定
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///post.db"
@@ -15,6 +22,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(100))  # 画像のファイル名
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 # JSTフィルター
@@ -27,38 +35,58 @@ def to_jst(utc_dt):
 with app.app_context():
     db.create_all()
 
-# ルーティング
+# トップページ
 @app.route('/')
 def index():
     posts = Post.query.order_by(Post.created_at.desc()).all()
     return render_template('index.html', posts=posts)
 
+# 投稿詳細
 @app.route('/post/<int:post_id>')
 def post_detail(post_id):
     post = Post.query.get_or_404(post_id)
-    return render_template('post_detail.html', post=post, request=request)
+    return render_template('post_detail.html', post=post)
 
+# 新規作成
 @app.route('/new', methods=['GET', 'POST'])
 def new_post():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        post = Post(title=title, content=content)
+        image = request.files.get('image')
+
+        filename = None
+        if image and image.filename != '':
+            filename = secure_filename(image.filename)
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        post = Post(title=title, content=content, image=filename)
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('index'))
+
     return render_template('new_post.html')
 
-@app.route('/edit/<int:post_id>', methods=['GET', 'POST'])
-def edit_post(post_id):
-    post = Post.query.get_or_404(post_id)
+# 編集
+@app.route('/edit/<int:id>', methods=['GET', 'POST'], endpoint='edit_post')
+def edit_post(id):
+    post = Post.query.get_or_404(id)
     if request.method == 'POST':
         post.title = request.form['title']
         post.content = request.form['content']
+        
+        image = request.files.get('image')
+        if image and image.filename != '':
+            filename = secure_filename(image.filename)
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            post.image = filename
+
         db.session.commit()
-        return redirect(url_for('post_detail', post_id=post.id))
+        return redirect(url_for('index'))
+
     return render_template('edit_post.html', post=post)
 
+# 削除
 @app.route('/delete/<int:post_id>', methods=['POST'])
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
